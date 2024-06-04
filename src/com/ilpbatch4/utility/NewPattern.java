f2ad02027eaef0b9932e157adcf8b522c8f775c6
RiyaJose1207 <riyamaryjose12@gmail.com>
2024-06-04 15:26:47 +0530

    NewPattern

---
+++ src/com/ilpbatch4/utility/NewPattern.java
@@ -0,0 +1,20 @@
+// package com.ilpbatch4.utility;
+
+public class NewPattern {
+
+	public static void main(String[] args) {
+		int input=4;
+		newPattern(input); 
+
+	}
+	private static void newPattern(int input) {
+		for(int i=1;i<=input;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("*"+" ");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
